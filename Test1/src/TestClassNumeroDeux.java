0fafb04c6395d24aba4a9a1ab24c4e4ef28cb4ae
JPL.Lloyd <JPL.Lloyd@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-03-16 13:56:08 +0000

    Addition

---
+++ Test1/src/TestClassNumeroDeux.java
@@ -0,0 +1,13 @@
+/**
+ * TestClassNumeroDeux
+ * 
+ * Integral part which does some important thing very good and cannot be deleted, ever!
+ * @author jesper
+ *
+ */
+public class TestClassNumeroDeux {
+	
+	
+	public void doesStuff(){}
+
+}
