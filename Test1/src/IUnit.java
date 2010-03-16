699198a7658dfa53bbac3e72aaf360c500b14b7e
lumboo.andersson <lumboo.andersson@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-03-16 14:56:12 +0000

    IUser, the interface for the common unit in game. New Interface upload

---
+++ Test1/src/IUnit.java
@@ -0,0 +1,10 @@
+
+public interface IUnit {
+	public void setHealth(int health);
+	public int getHealth();
+	public void kill();
+	public void revive();
+	//public void setWeapon(Weapon wep);
+	public void setWeaponDamage(int damage);
+	public int getWeaponDamage();
+}
