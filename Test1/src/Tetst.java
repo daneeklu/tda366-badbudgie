8c7dcc00c5e506b6c10d7f8af29141c306d79637
lumboo.andersson <lumboo.andersson@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-03-16 13:52:25 +0000

    jkhasd

--- Test1/src/Tetst.java
+++ Test1/src/Tetst.java
@@ -3,6 +3,7 @@ public class Tetst {
 	public static void main(String [] arg){
 	
 		System.out.println("jdkf");
+		//BUT I WILL!!!!!!
 		//I WILL NOT LEAVE CODE UNDOCUMENTED!
 		//I WILL NOT LEAVE CODE UNDOCUMENTED!
 		//I WILL NOT LEAVE CODE UNDOCUMENTED!
