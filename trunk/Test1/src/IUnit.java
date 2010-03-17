3be10e854537c6d1c589166267664f60e6875d36
JPL.Lloyd <JPL.Lloyd@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-03-17 18:53:13 +0000

    Documented

--- trunk/Test1/src/IUnit.java
+++ trunk/Test1/src/IUnit.java
@@ -1,10 +1,37 @@
-
+/**
+ * Interface implemented by classes representing non-static units in the game world.
+ * @author andreas
+ *
+ */
 public interface IUnit {
+	/**
+	 * Sets the health of the unit.
+	 * @param health the integer value giving the new health.
+	 */
 	public void setHealth(int health);
+	/**
+	 * Returns the integer value representing the current health
+	 * of the unit.
+	 * @return the current health of the unit
+	 */
 	public int getHealth();
+	/**
+	 * Renders the unit inactive and invulnerable.
+	 */
 	public void kill();
+	/**
+	 * Restores the unit to an operating mode.
+	 */
 	public void revive();
-	//public void setWeapon(Weapon wep);
+
+	/**
+	 * Sets the damage that the weapon of the unit will inflict.
+	 * @param damage the integer value representing the damage.
+	 */
 	public void setWeaponDamage(int damage);
+	/**
+	 * Returns the current damage of the units weapon.
+	 * @return the integer value representing the damage.
+	 */
 	public int getWeaponDamage();
 }
