c84aecf0b82d786e0bbb5ace59ee81603889a5df
dkvarfordt <dkvarfordt@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-04-21 16:08:39 +0000

    Collision handler

--- trunk/BadBudgie/src/edu/chl/tda366badbudgie/core/physics/IPhysics.java
+++ trunk/BadBudgie/src/edu/chl/tda366badbudgie/core/physics/IPhysics.java
@@ -3,5 +3,5 @@ package edu.chl.tda366badbudgie.core.physics;
 import edu.chl.tda366badbudgie.core.GameRound;
 
 public interface IPhysics {
-	public void doPhysics(GameRound gr);
+	public void doPhysics();
 }
