fb6a3f0f3f430a5e5d6b8d94b2b447121d59cc11
lumboo.andersson <lumboo.andersson@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-04-21 08:57:00 +0000

    Main updated to show the frame.

--- trunk/BadBudgie/src/edu/chl/tda366badbudgie/Main.java
+++ trunk/BadBudgie/src/edu/chl/tda366badbudgie/Main.java
@@ -1,8 +1,10 @@
 package edu.chl.tda366badbudgie;
 
+import edu.chl.tda366badbudgie.gui.GraphicsFrame;
+
 
 public class Main {
 	public static void main(String[] args) {
-		//TODO: Add main here
+		new GraphicsFrame();
 	}
 }
