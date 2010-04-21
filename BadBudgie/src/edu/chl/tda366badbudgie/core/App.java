f3b8cc38200645d07591b6800c1c251c4a945474
dkvarfordt <dkvarfordt@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-04-21 20:29:58 +0000

    Implemented KeyController

--- BadBudgie/src/edu/chl/tda366badbudgie/core/App.java
+++ BadBudgie/src/edu/chl/tda366badbudgie/core/App.java
@@ -1,8 +1,19 @@
 package edu.chl.tda366badbudgie.core;
 
 public class App {
-
+	
 	public void doLogic() {
 		//TODO: add logic here
+	}
+
+	public void buttonStateChanged(String id, boolean down) {
+		if (down) {
+			System.out.println("button pressed:" + id);			
+		}
+		else {
+			System.out.println("button released:" + id);
+		}
+		
+		
 	}
 }
--- BadBudgie/src/edu/chl/tda366badbudgie/ctrl/KeyController.java
+++ BadBudgie/src/edu/chl/tda366badbudgie/ctrl/KeyController.java
@@ -1,5 +1,58 @@
 package edu.chl.tda366badbudgie.ctrl;
 
-public class KeyController{
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Arrays;
+import java.util.HashMap;
 
+import edu.chl.tda366badbudgie.core.App;
+
+/**
+ * Controller class for keyboard input.
+ * 
+ * @author tda366-badbudgie
+ *
+ */
+public class KeyController extends KeyAdapter{
+
+	private App app;
+	private HashMap<Integer, String> keyMap;
+	private boolean[] keyDown;
+	
+	public KeyController(App app) {
+		this.app = app;
+		keyMap = new HashMap<Integer, String>();
+		keyDown = new boolean[500]; 				// TODO: find exact value needed 
+		Arrays.fill(keyDown, false);
+		
+		// temp key bindings, will be loaded from xml later
+		keyMap.put(KeyEvent.VK_LEFT, "left");
+		keyMap.put(KeyEvent.VK_RIGHT, "right");
+		keyMap.put(KeyEvent.VK_UP, "up");
+		keyMap.put(KeyEvent.VK_DOWN, "down");
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent ke) {
+		
+		int kc = ke.getKeyCode();
+		if (keyMap.containsKey(kc) && !keyDown[kc]) {
+			app.buttonStateChanged(keyMap.get(kc), true);
+			keyDown[kc] = true;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent ke) {
+		
+		int kc = ke.getKeyCode();
+		if (keyMap.containsKey(kc) && keyDown[kc]) {
+			app.buttonStateChanged(keyMap.get(kc), false);
+			keyDown[kc] = false;
+		}
+		
+	}
+	
 }
