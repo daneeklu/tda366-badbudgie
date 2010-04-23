8efa4a7f3025c864cb507bbaf107af186d5bba15
dkvarfordt <dkvarfordt@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-04-23 15:42:08 +0000

    Added shutdown method to IStateContext and GraphicsFrame, called from MenuState.

--- trunk/BadBudgie/src/edu/chl/tda366badbudgie/ctrl/IStateContext.java
+++ trunk/BadBudgie/src/edu/chl/tda366badbudgie/ctrl/IStateContext.java
@@ -1,4 +1,5 @@
 package edu.chl.tda366badbudgie.ctrl;
+import edu.chl.tda366badbudgie.gui.GraphicsFrame;
 import edu.chl.tda366badbudgie.gui.graphics.IGraphics;
 
 public interface IStateContext {
@@ -11,5 +12,7 @@ public interface IStateContext {
 		public void setState(String string);
 		
 		public void shutDown();
+
+		public void setFrame(GraphicsFrame graphicsFrame);
 		
 }
