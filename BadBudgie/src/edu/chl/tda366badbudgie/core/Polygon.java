281ac5366bfc40453e9b5871ea3996356c40319a
JPL.Lloyd <JPL.Lloyd@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-04-25 18:09:40 +0000

    Comment edit. Code cleanup. Slight performance improvement.

--- BadBudgie/src/edu/chl/tda366badbudgie/core/Polygon.java
+++ BadBudgie/src/edu/chl/tda366badbudgie/core/Polygon.java
@@ -9,32 +9,35 @@ import java.util.List;
  * 
  * A convex 2-dimensional polygon.
  * 
- * @author dkvarfordt
- *
+ * @author dkvarfordt, jesper
+ * 
  */
 public class Polygon {
-	
+
 	protected ArrayList<Vector> vertices;
-	
+
 	/**
-	 * Creates a new polygon with the given 
-	 * list of Vector2D objects as vertices.
-	 * 
-	 * @param vertices the list of vertices
+	 * Creates a new polygon with the given list of Vector2D objects as
+	 * vertices.
+	 * The vertices must form a simple, convex polygon.
+	 * A list of invalid vertices will throw an IllegalArgumentException.
+	 * @param vertices
+	 *            the list of vertices
 	 */
 	public Polygon(List<Vector> vertices) {
 
-		if(!checkConvexity(vertices)){
+		if (!checkConvexity(vertices)) {
 			throw new IllegalArgumentException();
-		} else if(!checkCCW(vertices)){
+		} else if (!checkCCW(vertices)) {
 			Collections.reverse(vertices);
 		}
 		this.vertices = new ArrayList<Vector>(vertices.size());
 		this.vertices.addAll(vertices);
 	}
-	
-	protected Polygon(){}
-	
+
+	protected Polygon() {
+	}
+
 	/**
 	 * Checks whether the supplied vertices form a convex polygon.
 	 * 
@@ -43,41 +46,39 @@ public class Polygon {
 	protected static boolean checkConvexity(List<Vector> vertices) {
 		if (vertices.size() < 3) {
 			return false;
-		} 
+		}
 		double oldSign = 0;
 		boolean changedDirection = false;
-		
+
 		for (int i = 0; i < vertices.size(); i++) {
-			
-			
+
 			Vector aVec = vertices.get(i);
 			Vector bVec = vertices.get((i + 1) % vertices.size());
 			Vector cVec = vertices.get((i + 2) % vertices.size());
-			
+
 			if (aVec.equals(bVec) || aVec.equals(cVec)) {
 				return false;
 			}
-				
+
 			Vector nV1 = bVec.subtract(aVec);
 			Vector nV2 = cVec.subtract(bVec);
-			
-			if(!nV1.sameDirection(nV2)){
+
+			if (!changedDirection && !nV1.sameDirection(nV2)) {
 				changedDirection = true;
 			}
-			
-			if(nV1.oppositeDirection(nV2)){
+
+			if (nV1.oppositeDirection(nV2)) {
 				return false;
 			}
-			
+
 			double newSign = Math.signum(nV1.crossProduct(nV2));
-			
-			if(oldSign != 0 && newSign != 0 && oldSign != newSign)
-			{
+
+			if (oldSign != 0 && newSign != 0 && oldSign != newSign) {
 				return false;
 			}
-			
+
 			oldSign = newSign;
-			
+
 		}
 		return changedDirection;
 	}
@@ -87,25 +88,22 @@ public class Polygon {
 	 * 
 	 * @return true, if the order is counter clockwise, otherwise false.
 	 */
-	protected static boolean checkCCW(List<Vector> vertices){
-		
+	protected static boolean checkCCW(List<Vector> vertices) {
+
 		if (vertices.size() < 3) {
 			return false;
-		} 
-		
+		}
+
 		Vector aVec = vertices.get(0);
 		Vector bVec = vertices.get(1);
 		Vector cVec = vertices.get(2);
-		
+
 		Vector nV1 = bVec.subtract(aVec);
 		Vector nV2 = cVec.subtract(bVec);
-		
+
 		return nV1.crossProduct(nV2) > 0;
 	}
-	
-	
-	
-	
+
 	/**
 	 * Returns a list of the vertices representing the polygon.
 	 * 
@@ -116,6 +114,5 @@ public class Polygon {
 		r.addAll(vertices);
 		return r;
 	}
-	
-	
+
 }
