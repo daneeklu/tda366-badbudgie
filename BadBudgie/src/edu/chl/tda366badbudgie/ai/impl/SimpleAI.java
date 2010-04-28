4fce3044e5cbb0144b158cad15b2cc4157743f19
lumboo.andersson <lumboo.andersson@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-04-28 21:32:33 +0000

    Added some AI in SimpleAI Updates made on Enemy Updates in GameRound New class called Weapon, for testing weapons. Added some more files for the FileHandler to read. Some minor changes in MovementHandler

--- BadBudgie/src/edu/chl/tda366badbudgie/ai/impl/SimpleAI.java
+++ BadBudgie/src/edu/chl/tda366badbudgie/ai/impl/SimpleAI.java
@@ -1,5 +1,50 @@
 package edu.chl.tda366badbudgie.ai.impl;
 
+import edu.chl.tda366badbudgie.core.Enemy;
+import edu.chl.tda366badbudgie.core.Vector;
+
 public class SimpleAI {
+	private Enemy enemy;
+	private Vector controlPosition = new Vector(9999, 9999);
+	public SimpleAI(Enemy enemy){
+		this.enemy = enemy;
+		enemy.applyForce(new Vector(5, 0));
+		enemy.moveRight(true);
+	}
+
+	public void calculateMove() {
+		
+		Vector v = enemy.getGroundContactVector();
+		
+		if(new Vector(4, 0).dotProduct(v) == 0 || new Vector(-4, 0).dotProduct(v) == 0){
+			
+			if(enemy.getDirection().equals("left") && posControl()){
+				System.out.println("Left");
+				setControlPosition();
+				enemy.moveLeft(false);
+				enemy.moveRight(true);
+			}
+			else if(enemy.getDirection().equals("right") && posControl()){
+				System.out.println("Right");
+				setControlPosition();
+				enemy.moveLeft(true);
+				enemy.moveRight(false);
+			}
+		}
+	}
+	
+	public void setControlPosition(){
+		controlPosition = enemy.getPosition();
+		System.out.println(controlPosition);
+	}
+	
+	
+	public boolean posControl(){
+		double d = controlPosition.getX() - enemy.getX();
+		if(d > 10 || d < -10){
+			return true;
+		}
+		return false;
+	}
 
 }
--- BadBudgie/src/edu/chl/tda366badbudgie/physics/MovementHandler.java
+++ BadBudgie/src/edu/chl/tda366badbudgie/physics/MovementHandler.java
@@ -29,7 +29,7 @@ public class MovementHandler {
 			ago.setForce(new Vector(0, 0));
 			
 		}
-		
+
 	}
 
 }
