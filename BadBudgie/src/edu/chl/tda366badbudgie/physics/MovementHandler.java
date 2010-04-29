a1d71862aebd49540e43b2dbbb2cbf7a6e2a79b6
dkvarfordt <dkvarfordt@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-04-29 19:46:59 +0000

    Added AGO LevelExit.

--- BadBudgie/src/edu/chl/tda366badbudgie/physics/MovementHandler.java
+++ BadBudgie/src/edu/chl/tda366badbudgie/physics/MovementHandler.java
@@ -14,19 +14,22 @@ public class MovementHandler {
 		
 		for(AbstractGameObject ago : gameObjects) {
 			
-			ago.updateForces();
-			
-			// Apply gravity
-			ago.applyForce(new Vector(0, -Physics.g * ago.getMass()));
-			
-			// Set the new speed of the object
-			ago.setVelocity(ago.getVelocity().add(ago.getForce().scalarDivision(ago.getMass())));
-			
-			// Set the new position of the object
-			ago.setPosition(ago.getPosition().add(ago.getVelocity()));
-			
-			// Reset the force on the object
-			ago.setForce(new Vector(0, 0));
+			if (!ago.isStationary()) {
+				// Update the external forces acting on the object
+				ago.updateForces();
+				
+				// Apply gravity
+				ago.applyForce(new Vector(0, -Physics.g * ago.getMass()));
+				
+				// Set the new speed of the object
+				ago.setVelocity(ago.getVelocity().add(ago.getForce().scalarDivision(ago.getMass())));
+				
+				// Set the new position of the object
+				ago.setPosition(ago.getPosition().add(ago.getVelocity()));
+				
+				// Reset the force on the object
+				ago.setForce(new Vector(0, 0));
+			}
 			
 		}
 
