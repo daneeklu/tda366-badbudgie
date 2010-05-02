ecd446458707f8ed7dec5adf26a68e221950a6aa
JPL.Lloyd <JPL.Lloyd@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-05-02 14:17:36 +0000

    New class Circle, inherits polygon. Allows for quick creation of approximated circles with variable resolution.

---
+++ trunk/BadBudgie/src/edu/chl/tda366badbudgie/core/Circle.java
@@ -0,0 +1,25 @@
+package edu.chl.tda366badbudgie.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Circle extends Polygon {
+	
+	public Circle(double radius, int resolution){
+		super(circleVertices(radius, resolution));
+	}
+	
+	private static List<Vector> circleVertices(double radius, int resolution){
+		
+		List<Vector> list = new ArrayList<Vector>(resolution);
+		
+		//Currently centered in origo
+		for(int i = 0; i < resolution; i++){
+			double x = radius * Math.cos(i*((Math.PI*2)/resolution)); // + radius;
+			double y = radius * Math.sin(i*((Math.PI*2)/resolution)); // + radius;
+			Vector v = new Vector(x, y);
+			list.add(v);
+		}
+		return list;
+	}
+}
