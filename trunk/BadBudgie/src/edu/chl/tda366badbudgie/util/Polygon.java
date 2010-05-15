fc3a045ec5593f1100449fd5c726cb890076f71b
JPL.Lloyd <JPL.Lloyd@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-05-15 07:29:34 +0000

    Moved screen variables to new class Screen. Moved point-in-polygon check to Polygon. Deleted StaticUtils, moved unit test to Polygon.

--- trunk/BadBudgie/src/edu/chl/tda366badbudgie/util/Polygon.java
+++ trunk/BadBudgie/src/edu/chl/tda366badbudgie/util/Polygon.java
@@ -93,6 +93,61 @@ public class Polygon {
 	}
 
 	/**
+	 * Checks to see if the point given is inside the specified polygon.
+	 * 
+	 * @param point the point to check
+	 * @param p the polygon to check
+	 * @return true if the point is inside
+	 */
+	public static boolean isPointInPolygon(Vector point, Polygon p) {
+		
+		ArrayList<Vector> pVerts = new ArrayList<Vector>(p.getVertices());
+		
+		ArrayList<Vector> normals = new ArrayList<Vector>();
+		
+		// Add normals of a to the list of normals
+		for (int i = 0; i < pVerts.size(); i++) {
+
+			Vector p1, p2;
+
+			p1 = pVerts.get(i);
+			p2 = pVerts.get((i + 1 == pVerts.size()) ? 0 : i + 1);
+
+			Vector normal = p2.subtract(p1).perpendicularCCW();
+			normal = normal.normalize();
+
+			normals.add(normal);
+		}
+		
+		
+		for (Vector normal : normals) {
+
+			double min = pVerts.get(0).dotProduct(normal);
+			double max = min;
+
+			// Project each vertex of a onto the current normal and save the min
+			// and max value
+			for (Vector v : pVerts) {
+				double dot = v.dotProduct(normal);
+				if (dot < min) {
+					min = dot;
+				} else if (dot > max) {
+					max = dot;
+				}
+			}
+			
+			double pointProj = point.dotProduct(normal);
+			
+			if (pointProj <= min || pointProj >= max)
+				return false;
+			
+		}
+		
+		return true;
+	}
+
+	
+	/**
 	 * Checks whether the polygon is CCW-ordered.
 	 * 
 	 * @return true, if the order is counter clockwise, otherwise false.
@@ -170,5 +225,5 @@ public class Polygon {
 	public Vector getBoundingBoxPosition() {
 		return boundingBoxPosition;
 	}
-
+	
 }
