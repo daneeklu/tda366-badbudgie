47114bde660e4512982aed344880164847dd964e
dkvarfordt <dkvarfordt@224fc49a-3050-11df-8971-ebb891de2ff2>
2010-05-18 13:42:59 +0000

    Removed unused import

--- BadBudgie/src/edu/chl/tda366badbudgie/core/game/Player.java
+++ BadBudgie/src/edu/chl/tda366badbudgie/core/game/Player.java
@@ -7,7 +7,6 @@ import edu.chl.tda366badbudgie.core.game.GameRound.GameRoundMessage;
 import edu.chl.tda366badbudgie.util.Animation;
 import edu.chl.tda366badbudgie.util.Polygon;
 import edu.chl.tda366badbudgie.util.Sprite;
-import edu.chl.tda366badbudgie.util.Screen;
 import edu.chl.tda366badbudgie.util.Vector;
 
 /**
